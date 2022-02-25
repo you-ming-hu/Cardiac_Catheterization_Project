953700087c03e15d62b8d021db3d6b4e720dfb94
atlas <atlas0435@gmail.com>
2022-02-25 23:50:27 +0800

    daily commit

--- pytorch/Transform/Preprocess.py
+++ pytorch/Transform/Preprocess.py
@@ -3,4 +3,5 @@ from albumentations.pytorch.transforms import ToTensorV2
 
 v1 = A.Sequential([
     A.CLAHE(clip_limit=4,tile_grid_size=(8,8),always_apply=False,p=1),
+    A.ToFloat(p=1),
     ToTensorV2(p=1)],p=1)
