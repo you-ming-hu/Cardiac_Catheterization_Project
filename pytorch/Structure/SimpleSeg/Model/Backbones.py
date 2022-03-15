dcaa29bdc3cd9257cadd39996af3e1d1a88fa930
atlas <atlas0435@gmail.com>
2022-03-15 19:42:06 +0800

    fix torchvision bug like the previous commit and fix the bug of smp models while using different encoders

--- pytorch/Structure/SimpleSeg/Model/Backbones.py
+++ pytorch/Structure/SimpleSeg/Model/Backbones.py
@@ -4,28 +4,32 @@ import segmentation_models_pytorch as smp
 
 class torchvision_FCN_ResNet(BaseBackbone):
     def __init__(self, pretrained, out_dim):
-        
-        stem = tv.models.segmentation.fcn_resnet50(pretrained=pretrained,num_classes=out_dim,aux_loss=None)
-        non_frozen_layers = [stem.backbone.con1, stem.backbone.bn1, stem.classifier[-1]]
+        stem = tv.models.segmentation.fcn_resnet50(pretrained=False,num_classes=out_dim,aux_loss=None,pretrained_backbone=pretrained)
+        non_frozen_layers = [stem.backbone.conv1, stem.backbone.bn1, stem.classifier[-1]]
         
         super().__init__(stem,non_frozen_layers)
         
     def forward(self, x):
         x = self.stem(x)
+        x = x['out']
         return x
     
 class smp_Unet(BaseBackbone):
-    def __init__(self, encoder_name, encoder_weights, in_channels, activation, output_dim):
+    def __init__(self, encoder_name, encoder_weights, in_channels, output_dim):
         
         stem = smp.Unet(
             encoder_name = encoder_name, 
             encoder_weights = encoder_weights, 
             in_channels = in_channels, 
             classes = output_dim,
-            activation = activation,
+            activation = None,
             aux_params = None)
-        non_froozen_layers = [stem.encoder.conv1, stem.encoder.bn1, stem.segmentation_head]
-        
+        if encoder_name.lower().startswith('resnet'):
+            non_froozen_layers = [stem.encoder.conv1, stem.encoder.bn1, stem.segmentation_head]
+        elif encoder_name.lower().startswith('mobilenet'):
+            non_froozen_layers = [stem.encoder.features[0][0], stem.encoder.features[0][1], stem.segmentation_head]
+        else:
+            assert False
         super().__init__(stem,non_froozen_layers)
         
     def forward(self, x):
