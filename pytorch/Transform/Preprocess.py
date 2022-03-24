61f787c5561d5b461fd3aa5bd77309302e1577e1
atlas <atlas0435@gmail.com>
2022-03-24 17:19:25 +0800

    fix a bug by removing the randomness in preprocess

--- pytorch/Transform/Preprocess.py
+++ pytorch/Transform/Preprocess.py
@@ -1,4 +1,4 @@
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
-v1 = A.CLAHE(clip_limit=4,tile_grid_size=(8,8),always_apply=False,p=1)
+v1 = A.CLAHE(clip_limit=(4,4),tile_grid_size=(8,8),always_apply=False,p=1)
