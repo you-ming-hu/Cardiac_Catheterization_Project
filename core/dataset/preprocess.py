0f95102d219e6f8c6f18823652e06f5e338f7439
atlas <atlas0435@gmail.com>
2022-06-13 21:28:38 +0800

    daily commit

--- core/dataset/preprocess.py
+++ core/dataset/preprocess.py
@@ -4,18 +4,24 @@ def PreprocessParser(func_str):
     return eval(func_str)
 
 def GrayImage(image):
+    if image.shape[1:] != (512,512):
+        image = cv2.resize(image,(512,512))
     if len(image.shape) == 2:
         return image
     else:
         return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 def RGBImage(image):
+    if image.shape[1:] != (512,512):
+        image = cv2.resize(image,(512,512))
     if len(image.shape) == 2:
         return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
     else:
         return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
 def BGRImage(image):
+    if image.shape[1:] != (512,512):
+        image = cv2.resize(image,(512,512))
     if len(image.shape) == 2:
         return cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     else:
