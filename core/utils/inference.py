2fa6378ec599f9eff32d7ef871b5dfb3f2b92ae0
atlas <atlas0435@gmail.com>
2022-06-15 11:03:26 +0800

    daily commit

--- core/utils/inference.py
+++ core/utils/inference.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pathlib
+import numpy as np
+
 
 def SegWithClass(save_root_path,batch_data,output):
     batch_size = batch_data['image'].shape[0]
@@ -20,14 +22,20 @@ def SegWithClass(save_root_path,batch_data,output):
         pred_mask = output['mask'][i]
         pred_contrast_exist =  output['contrast_exist'][i]
         
-        save_path = pathlib.Path(save_root_path,sample_id,frame_id)
-        save_path.mkdir(parents=True,exist_ok=True)
-        
-        plt.imsave(save_path.joinpath(f'prev.png'),prev_img,cmap='gray')
-        plt.imsave(save_path.joinpath(f'curr.png'),curr_img,cmap='gray')
-        plt.imsave(save_path.joinpath(f'mask_{int(contrast_exist):d}.png'),mask,cmap='gray')
-        
-        plt.imsave(save_path.joinpath(f'pred_{pred_contrast_exist:.4f}.png'),pred_mask,cmap='gray')
+        if contrast_exist == 1:
+            need_save = bool(np.random.binomial(1,0.33))
+        else:
+            need_save = bool(np.random.binomial(1,0.1))
+            
+        if need_save:
+            save_path = pathlib.Path(save_root_path,sample_id,frame_id)
+            save_path.mkdir(parents=True,exist_ok=True)
+            
+            plt.imsave(save_path.joinpath(f'prev.png'),prev_img,cmap='gray')
+            plt.imsave(save_path.joinpath(f'curr.png'),curr_img,cmap='gray')
+            plt.imsave(save_path.joinpath(f'mask_{int(contrast_exist):d}.png'),mask,cmap='gray')
+            
+            plt.imsave(save_path.joinpath(f'pred_{pred_contrast_exist:.4f}.png'),pred_mask,cmap='gray')
         
         
         
